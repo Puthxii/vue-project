c25f4954d5dbd953d1809b9cf70ef2f6367c0710
puthxii <teenaputh12@gmail.com>
2020-04-17 14:36:33 +0700

    Separate components

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,42 @@
     <h1>My online store</h1>
     
     <div class="row">
-      <div class="col-md-3" v-for="item in forSale">
-        <div class="card">
-          <img :src="item.image" :alt="item.name" class="card-img-top">
-          <div class="card-body">
-            <h4 class="card-title">{{ item.name }}</h4>
-            <div class="card-text">${{ item.price / 100 }}</div>
-            <div class="row justify-content-end">
-              <button class="btn btn-primary">Add to cart</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Item
+        v-for="item in forSale"
+        :key="item.invId"
+        :invId="item.invId"
+        :name="item.name"
+        :des="item.des"
+        :image="item.image"
+        :price="item.price" />
     </div>
   </div>
 </template>
 <script>
+import Item from './Item.vue';
 
 export default {
   name: 'app',
   data() {
     return {
       forSale: [
-        { invId: 1, name: 'An Item', image: '//placehold.it/200', price: 999 },
-        { invId: 2, name: 'Thing', image: '//placehold.it/200', price: 1499 },
-        { invId: 3, name: 'Doo-dad', image: '//placehold.it/200', price: 499 },
-        { invId: 4, name: 'Other thing', image: '//placehold.it/200', price: 299 },
+        { invId: 1, name: 'Quinoa Fried Rice & Chilli Dressing',
+          des: "Nourishing Quinoa Fried Rice. Serve with Steamed Giant Seabass and spicy seafood sauce.",
+          image: 'https://images.snpfood.com/image/upload/c_fill,h_300,q_auto,w_485/v1/brands/15/inventory/products/16223--9Vvh44', price: 752 },
+        { invId: 2, name: 'Roast Chicken & white sauce Salad', 
+          des: "Organic salad vegetables are grown in non-toxic farms. They are fresh and ready for cook every day.",
+          image: 'https://images.snpfood.com/image/upload/c_fill,h_300,q_auto,w_485/v1/brands/15/inventory/products/18919--fT4VOB', price: 660 },
+        { invId: 3, name: 'Fried Rice Vermicelli Thai', 
+          des: "Top Recommended! Unique Thai noodle from Nonthaburi. Serve with Padthai traditional sauce.",
+          image: 'https://images.snpfood.com/image/upload/c_fill,h_300,q_auto,w_485/v1/brands/15/inventory/products/16259--L0wIOW', price: 445 },
+        { invId: 4, name: 'Strawberry Jelio Smoothie', 
+          des: "Strawberry Jelio Smoothie. With the additional fresh Strawberry & Yogurt whipped cream.",
+          image: 'https://images.snpfood.com/image/upload/c_fill,h_300,q_auto,w_485/v1/brands/15/inventory/products/24878--kFd3xN', price: 393 },
       ],
     };
+  },
+  components: {
+    Item,
   },
 };
 </script>
---
+++ src/Item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-md-3">
+    <div class="card">
+      <img :src="image" :alt="name" class="card-img-top">
+      <div class="card-body">
+        <h4 class="card-title">{{ name }}</h4>
+        <div class="card-subtitle">{{ des }}</div>
+        <div class="card-text">{{ price | dollars }}</div>
+        <div class="row justify-content-end">
+          <button class="btn btn-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'item',
+    props: ['invId', 'name', 'des', 'image', 'price'],
+    filters: {
+        dollars: num => `$${num / 100}`,
+    },
+
+    };
+</script>
